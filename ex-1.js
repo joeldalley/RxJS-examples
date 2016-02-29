edb9713e6bb3b0288a9f0513eec62e34a36df9b2
Joel Dalley <joeldalley@gmail.com>
2016-02-28 19:44:49 -0700

    Add merged stream.

--- ex-1.js
+++ ex-1.js
@@ -12,37 +12,37 @@ function input() {
     ];
 }
 
-function getArray() {
+function getPriceArray() {
     return input()
       .filter(quote => quote.price > 30)
       .map(quote => quote.price);
 }
 
-function getStream() {
+function getPriceStream(filter) {
     const data = input();
 
     return Rx.Observable
       .interval(300)
       .take(data.length)
-      .map(x => data[x]);
-}
-
-function filteredStream(stream, filter) {
-  return stream
-    .filter(filter)
-    .map(quote => quote.price);
+      .map(idx => data[idx])
+      .filter(filter)
+      .map(quote => quote.price);
 }
 
 console.log("Array:");
-getArray().forEach(price => console.log(`Prices higher than $30: ${price}`));
+getPriceArray().forEach(price => {
+    console.log(`Prices higher than $30: ${price}`);
+});
 console.log("");
 
-const stream = getStream();
+const lowPriceStream = getPriceStream(quote => quote.price <= 30);
+const highPriceStream = getPriceStream(quote => quote.price > 30);
+const mergedPriceStream = Rx.Observable.merge(lowPriceStream, highPriceStream);
 
 console.log("Streams:");
-filteredStream(stream, quote => quote.price <= 30)
+lowPriceStream
   .subscribe(price => console.log(`Prices at $30 or lower: ${price}`));
-
-filteredStream(stream, quote => quote.price > 30)
+highPriceStream
   .subscribe(price => console.log(`Prices higher than $30: ${price}`));
-
+mergedPriceStream
+  .subscribe(price => console.log(`Merged prices item: ${price}`));
